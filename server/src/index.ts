c43be898c04ed710ec8358880711dc26439c051f
sirasit <sirasit@siruxsolutions.com>
2022-03-06 00:22:47 -0800

    Finished MVP

--- server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
-
-createConnection().then(async connection => {
-    console.log("Hello Intern!");
-}).catch(error => console.log(error));
